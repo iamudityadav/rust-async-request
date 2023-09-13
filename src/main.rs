98c986c50fdd81cc94c80816af85b398b09cf8e7
Udit Yadav <askudityadav@gmail.com>
2023-09-13 22:25:14 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
+
+    println!("\nStatus: {}\n", res.status());
+    println!("Headers:\n{:#?}\n", res.headers());
+
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
+
+    Ok(())
+}
